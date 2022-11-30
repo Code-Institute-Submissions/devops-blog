370a37a94869d5050ad459eb6aa7971315f03044
Niall <niallos11@gmail.com>
2022-11-30 00:53:13 +0000

    Adjust buildpacks on Heroku

--- devops/settings.py
+++ devops/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6tu6c)xu@j$y)tjtpvze@x26r!#1chd6*))37!l5jsa%$&a4c&'  # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
